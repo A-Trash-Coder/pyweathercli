9e90f1c9f4db28106e47524c6a02d64dc0e9dff3
A-Trash-Coder <gavynlamar@gmail.com>
2020-05-23 16:59:00 -0400

    Add License and edit Setup

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
  
+with open("README.md", "r", encoding="utf8") as fh:
+    long_desc = fh.read()
+
 setup(
     name="pyweathercli",
-    version="1.0",
+    version="0.0.1",
+    author="Gavyn Stanley",
+    description="A cli using click for weather",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/A-Trash-Coder/pyweathercli",
     packages=find_packages(),
     include_package_data=True,
     package_data={'pyweathercli': ['*.json']},

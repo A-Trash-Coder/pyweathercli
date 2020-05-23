ee6e42f2bdcb05f16bae8826b3742af97c86d085
A-Trash-Coder <gavynlamar@gmail.com>
2020-05-23 16:32:15 -0400

    Trying to include .json

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0",
     packages=find_packages(),
     include_package_data=True,
-    package_data={'pyweathercli': ['config.json']},
+    package_data={'pyweathercli': ['*.json']},
     install_requires=["click", "pyfiglet", "pyowm", "colorama"],
     entry_points={
         "console_scripts":["weather=pyweathercli.cli:weather"]

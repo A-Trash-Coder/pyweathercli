0b4b7957f76d9753c016a2f8f7fafe6ae976f607
A-Trash-Coder <gavynlamar@gmail.com>
2020-05-23 16:21:39 -0400

    Edited init

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="1.0",
     packages=find_packages(),
     include_package_data=True,
-    data_files=[('config', ['pyweathercli\config.json'])],    
     install_requires=["click", "pyfiglet", "pyowm", "colorama"],
     entry_points={
         "console_scripts":["weather=pyweathercli.cli:weather"]
